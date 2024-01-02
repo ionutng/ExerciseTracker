7b9b0849549c348a9eec917656d13f7cbb8a0cb4
ionutng <ionutngp@gmail.com>
2024-01-02 21:02:31 +0200

    Update UserInterface and RunningController to adhere to the separation of concerns principle

--- ExerciseTracker/Controllers/RunningController.cs
+++ ExerciseTracker/Controllers/RunningController.cs
@@ -1,7 +1,5 @@
-﻿using ExerciseTracker.Models;
-using ExerciseTracker.Repositories;
+﻿using ExerciseTracker.Repositories;
 using ExerciseTracker.Services;
-using Spectre.Console;
 
 namespace ExerciseTracker.Controllers;
 
@@ -20,7 +18,8 @@ internal class RunningController(IRunningRepository runningRepository)
     {
         GetRuns();
 
-        int runId = AnsiConsole.Ask<int>("Enter the ID of the run:");
+        var runId = UserInterface.GetRunId();
+
         var run = _runningRepository.GetRunById(runId);
 
         UserInterface.ShowRun(run);
@@ -28,21 +27,7 @@ internal class RunningController(IRunningRepository runningRepository)
 
     public void AddRun()
     {
-        var run = new Running();
-
-        var startDate = AnsiConsole.Ask<DateOnly>("Date start (Format: MM-dd-yyyy):");
-        var endDate = AnsiConsole.Ask<DateOnly>("Date end (Format: MM-dd-yyyy):");
-        var startTime = AnsiConsole.Ask<TimeOnly>("Time start (Format: HH:mm:ss):");
-        var endTime = AnsiConsole.Ask<TimeOnly>("Time end (Format: HH:mm:ss):");
-
-        run.DateStart = new DateTime(startDate, startTime);
-        run.DateEnd = new DateTime(endDate, endTime);
-
-        run.Duration = run.DateEnd - run.DateStart;
-
-        run.Distance = AnsiConsole.Ask<float>("Distance:");
-
-        run.Comments = AnsiConsole.Ask<string>("Comments:");
+        var run = UserInterface.AskRunInfoInput();
 
         _runningRepository.AddRun(run);
     }
@@ -50,34 +35,12 @@ internal class RunningController(IRunningRepository runningRepository)
     public void UpdateRun()
     {
         GetRuns();
-        
-        int runId = AnsiConsole.Ask<int>("Enter the ID of the run:");
+
+        var runId = UserInterface.GetRunId();
 
         var run = _runningRepository.GetRunById(runId);
 
-        if (AnsiConsole.Confirm("Update start date?"))
-        {
-            var date = AnsiConsole.Ask<DateOnly>("New start date (Format: MM-dd-yyyy):");
-            var time = AnsiConsole.Ask<TimeOnly>("New start time (Format: HH:mm:ss):");
-            run.DateStart = new DateTime(date, time);
-        }
-
-        if (AnsiConsole.Confirm("Update end date?"))
-        {
-            var date = AnsiConsole.Ask<DateOnly>("New end date (Format: MM-dd-yyyy):");
-            var time = AnsiConsole.Ask<TimeOnly>("New end time (Format: HH:mm:ss):");
-            run.DateEnd = new DateTime(date, time);
-        }
-
-        run.Duration = run.DateEnd - run.DateStart;
-
-        run.Distance = AnsiConsole.Confirm("Update distance?")
-            ? run.Distance = AnsiConsole.Ask<float>("New distance:")
-            : run.Distance;
-
-        run.Comments = AnsiConsole.Confirm("Update comments?")
-            ? run.Comments = AnsiConsole.Ask<string>("Comments:")
-            : run.Comments;
+        UserInterface.UpdateRunInfoInput(run);
 
         _runningRepository.UpdateRun(run);
     }
@@ -86,7 +49,7 @@ internal class RunningController(IRunningRepository runningRepository)
     {
         GetRuns();
 
-        int runId = AnsiConsole.Ask<int>("Enter the ID of the run:");
+        var runId = UserInterface.GetRunId();
 
         var run = _runningRepository.GetRunById(runId);
 
--- ExerciseTracker/Services/UserInterface.cs
+++ ExerciseTracker/Services/UserInterface.cs
@@ -1,6 +1,5 @@
 ﻿using ExerciseTracker.Models;
 using Spectre.Console;
-using System.Text;
 using static ExerciseTracker.Models.Enums;
 
 namespace ExerciseTracker.Services;
@@ -91,5 +90,60 @@ static internal class UserInterface
         Console.WriteLine("Press any key to continue.");
         Console.ReadKey();
         Console.Clear();
+    }
+
+    static internal Running UpdateRunInfoInput(Running run)
+    {
+        if (AnsiConsole.Confirm("Update start date?"))
+        {
+            var date = AnsiConsole.Ask<DateOnly>("New start date (Format: MM-dd-yyyy):");
+            var time = AnsiConsole.Ask<TimeOnly>("New start time (Format: HH:mm:ss):");
+            run.DateStart = new DateTime(date, time);
+        }
+
+        if (AnsiConsole.Confirm("Update end date?"))
+        {
+            var date = AnsiConsole.Ask<DateOnly>("New end date (Format: MM-dd-yyyy):");
+            var time = AnsiConsole.Ask<TimeOnly>("New end time (Format: HH:mm:ss):");
+            run.DateEnd = new DateTime(date, time);
+        }
+
+        run.Duration = run.DateEnd - run.DateStart;
+
+        run.Distance = AnsiConsole.Confirm("Update distance?")
+            ? run.Distance = AnsiConsole.Ask<float>("New distance:")
+            : run.Distance;
+
+        run.Comments = AnsiConsole.Confirm("Update comments?")
+            ? run.Comments = AnsiConsole.Ask<string>("Comments:")
+            : run.Comments;
+
+        return run;
+    }
+
+    static internal Running AskRunInfoInput()
+    {
+        var run = new Running();
+
+        var startDate = AnsiConsole.Ask<DateOnly>("Date start (Format: MM-dd-yyyy):");
+        var endDate = AnsiConsole.Ask<DateOnly>("Date end (Format: MM-dd-yyyy):");
+        var startTime = AnsiConsole.Ask<TimeOnly>("Time start (Format: HH:mm:ss):");
+        var endTime = AnsiConsole.Ask<TimeOnly>("Time end (Format: HH:mm:ss):");
+
+        run.DateStart = new DateTime(startDate, startTime);
+        run.DateEnd = new DateTime(endDate, endTime);
+
+        run.Duration = run.DateEnd - run.DateStart;
+
+        run.Distance = AnsiConsole.Ask<float>("Distance:");
+
+        run.Comments = AnsiConsole.Ask<string>("Comments:");
+
+        return run;
+    }
+
+    static internal int GetRunId()
+    {
+        return AnsiConsole.Ask<int>("Enter the ID of the run:");
     }
 }
